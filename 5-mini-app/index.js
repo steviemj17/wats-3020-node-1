9c1616c97b546ce4f1ef07a43b9f0029fba13d80
steviemj17 <55863655+steviemj17@users.noreply.github.com>
2020-01-19 00:21:21 -0800

    part 5 done

--- 5-mini-app/index.js
+++ 5-mini-app/index.js
@@ -10,3 +10,13 @@
 // get an integer using getargs
 let getargs = require("../modules/getargs/index.js");
 let input = getargs.getIntegerArg();
+
+if (isNaN(input) || !Number.isInteger(input)) {
+    console.log("usage: node 5-mini-app <integer>");
+}
+
+else {
+    let dollars = Math.floor((input / 100));
+    let pennies = input % 100;
+    console.log('${input} pennies is ${dollars} dollars and ${pennies} cents.');
+}
