34476f1ef12d42016c6cb938e4addf0e0c93ceb0
steviemj17 <55863655+steviemj17@users.noreply.github.com>
2020-01-18 22:16:18 -0800

    part 2

--- 2-odd-even/index.js
+++ 2-odd-even/index.js
@@ -1,3 +1,4 @@
+
 /*
 The user should run this from the root of the project using `node 1-odd-even/index.js <integer> and should see the word odd or even depending on the integer.  If an integer is not provide, the user should see a usage statement reminding them of the how to run this code.
 */
@@ -8,16 +9,12 @@ let input = getargs.getIntegerArg();
 
 /**** Do not modify anything above this line ****/
 
-// test that input is a number
-// TODO use the if statement to check input using isNaN and NOT Number.isInteger 
-if () {
-  // TODO use console.log to provide a usage statement when user forgets to enter a number
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 2-odd-even <integer>");
 } else {
-  // TODO use the if statement to test if integer is divisible by 2
-  if () {
-    //TODO use console.log with a template literal to report that the number is even
-
+   if (input % 2 == 0) {
+console.log('$(input) is even');
   } else {
-    //TODO use console.log with a template literal to report that the number is odd
+    console.log('$(input) is odd');
   }
 }
